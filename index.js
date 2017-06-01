3bceb9a68693c7542fe604a4ee44f47db8e6b4d2
qianyipeng <loveqianwithyang@gmail.com>
2017-06-01 10:01:46 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zhouhongchun on 2017/6/1.
+ */
+export default class Index extends Component {
+    render() {
+        return (
+            // 试试去掉父View中的`flex: 1`。
+            // 则父View不再具有尺寸，因此子组件也无法再撑开。
+            // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
+            <View style={{height: 100,flexDirection: 'row'}}>
+                <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
